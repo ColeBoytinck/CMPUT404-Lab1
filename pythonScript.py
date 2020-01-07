bd24797ccde9b9bc38a082b046728df526b0b1cb
Cole Boytinck <cboytinc@cs.ualberta.ca>
2020-01-07 12:13:33 -0700

    Changed GET URL

--- pythonScript.py
+++ pythonScript.py
@@ -1,3 +1,3 @@
 # Source: http://xahlee.info/python/get_url.html
 import urllib.request
-print(urllib.request.urlopen("https://www.google.com/").read())
+print(urllib.request.urlopen("https://raw.githubusercontent.com/ColeBoytinck/CMPUT404-Lab1/master/pythonScript.py").read())
